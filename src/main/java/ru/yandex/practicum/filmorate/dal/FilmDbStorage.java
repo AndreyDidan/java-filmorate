3bf89ea36aad31f56d14af19f0541fa8f17fa864
Andrey Didan <andrey_didan@mail.ru>
2024-09-24 23:08:40 +0300

    Поправил все замечания кроме одного

--- src/main/java/ru/yandex/practicum/filmorate/dal/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/FilmDbStorage.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.dal;
 
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
